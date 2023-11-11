7117d0c4dbd288e572de284c730fbd23b7558d24
twozero28 <28twozero@gmail.com>
2023-11-11 12:29:52 +0900

    Reading Plug part

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -4,4 +4,8 @@ defmodule HelloWeb.HelloController do
   def index(conn, _params) do
     render(conn, :index)
   end
+
+  def show(conn, %{"messenger" => messenger} = params) do
+    render(conn, :show, messenger: messenger)
+  end
 end
