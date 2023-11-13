5dca8db99467f2c6dc9ae516f12595ae43eaef60
twozero28 <28twozero@gmail.com>
2023-11-13 23:34:15 +0900

    Reading Routing Part...

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -1,11 +1,13 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
+
   def index(conn, _params) do
     render(conn, :index)
   end
 
-  def show(conn, %{"messenger" => messenger} = params) do
+  def show(conn, %{"messenger" => messenger}) do
     render(conn, :show, messenger: messenger)
   end
 end
