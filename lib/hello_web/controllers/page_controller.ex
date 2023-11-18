7ea31fc513117c5c641eb5b615fdd9b701b7e2a5
twozero28 <28twozero@gmail.com>
2023-11-19 00:44:29 +0900

    Plan to read Components and HEEx

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -5,6 +5,13 @@ defmodule HelloWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    conn
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> redirect(to: ~p"/redirect_test")
+  end
+
+  def redirect_test(conn, _params) do
     render(conn, :home, layout: false)
   end
+
 end
